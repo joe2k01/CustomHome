cc40780f3bb0cae00079d16bfc7d13f0284ff8d6
Giuseppe Barillari <joe2k01dev@gmail.com>
2021-10-04 21:41:47 +0100

    Initial Commit

---
+++ CustomHome/CustomHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHomeApp.swift
+//  CustomHome
+//
+//  Created by Giuseppe Barillari on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
